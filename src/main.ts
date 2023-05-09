c2598e0401d26ab4b12ece99ac58f15227ec0135
Damiyinliao <2911493102@qq.com>
2023-05-10 00:02:07 +0800

    :wrench:chore: 基础脚手架

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+
+import './style.css'
+
+import App from './App.vue'
+import { setupRouter } from '@/router';
+import { setupStore } from '@/store';
+const app = createApp(App);
+
+function setupApp() {
+    setupRouter(app);
+    setupStore(app);
+
+    app.mount('#app')
+}
+
+setupApp();
+
